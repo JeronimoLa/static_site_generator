af407a7829085bd3e83cffc762cfedfd74f4b469
JeronimoLa <81096816+JeronimoLa@users.noreply.github.com>
2025-09-26 11:24:16 -0500

    feat: split images and links into a textnode

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,8 @@
-import re
+import re, time
 from textnode import TextNode, TextType
+
+LINK_RE= re.compile(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)")
+IMAGE_RE = re.compile(r"\s*!\[(.+?)\]\((https:[^)]*)\)")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list[TextNode]:
     new_nodes= []
@@ -33,19 +36,81 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list[TextNode]:
     return new_nodes
 
 def extract_markdown_images(text):
-    pattern = r'\s+!\[(.+?)\]\((https:[^)]*)\)'
-    return [ match for match in re.findall(pattern, text) ]
+    return [ match for match in IMAGE_RE.findall(text) ]
 
 def extract_markdown_links(text): 
-    pattern = r'(?<!!)\[(.+?)\]\((https:[^)]*)\)'
-    return [ match for match in re.findall(pattern, text) ]
+    return [ match for match in LINK_RE.findall(text) ]
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        text = node.text
+        pos = 0
+        for m in LINK_RE.finditer(text):
+            start, end = m.span()
+            alt, url = m.group(1), m.group(2) 
+
+            if start > pos:
+                new_nodes.append(TextNode(text[pos:start], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.LINK, url))
+            pos = end
+
+        if pos < len(text):
+            new_nodes.append(TextNode(text[pos:], TextType.TEXT))
+
+    return new_nodes
+
+# def split_nodes_image(old_nodes):
+#     new_nodes = []
+#     for node in old_nodes:
+#         text = node.text
+#         pos = 0
+#         for m in IMAGE_RE.finditer(text):
+#             start, end = m.span()
+#             alt, url = m.group(1), m.group(2) 
+
+#             if start > pos:
+#                 new_nodes.append(TextNode(text[pos:start], TextType.TEXT))
+#             new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+#             pos = end
+
+#         if pos < len(text):
+#             new_nodes.append(TextNode(text[pos:], TextType.TEXT))
+
+#     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
 
 if __name__ == "__main__":
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    # text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_images(text)) # should print
-    print(extract_markdown_links(text)) # should not
- 
-    # print(extract_markdown_links(text))
-    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
-    
+    pass
